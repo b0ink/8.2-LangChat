90c5affd8ca2b445c2e3209a5d0881fbd433aed6
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-10 05:46:54 +1000

    Update build.gradle.kts
    Install rabbitmq client

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation("com.rabbitmq:amqp-client:5.21.0")
 //    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
 //    implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("com.auth0.android:jwtdecode:2.0.2")
