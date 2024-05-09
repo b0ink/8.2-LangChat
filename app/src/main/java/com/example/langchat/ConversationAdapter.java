1915a641eefaaccc87cdbafc830c8291ac78d7d0
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-10 00:56:27 +1000

    init conversations view

---
+++ app/src/main/java/com/example/langchat/ConversationAdapter.java
@@ -0,0 +1,93 @@
+package com.example.langchat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.langchat.API.AuthManager;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversationAdapter extends RecyclerView.Adapter<ConversationAdapter.ConversationViewHolder> {
+
+    public ArrayList<ConversationResponse> conversations;
+    private ArrayList<String> selectedInterests = new ArrayList<>();
+
+
+    public ConversationAdapter(Context context, ArrayList<ConversationResponse> conversations) {
+        this.conversations = conversations;
+    }
+
+
+    @NonNull
+    @Override
+    public ConversationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_conversation, parent, false);
+        return new ConversationViewHolder(view, selectedInterests);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConversationAdapter.ConversationViewHolder holder, int position) {
+        ConversationResponse conversation = conversations.get(position);
+        holder.bind(conversation);
+    }
+
+    @Override
+    public int getItemCount() {
+        return conversations.size();
+    }
+
+    public class ConversationViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvUsername;
+        private TextView tvLastMessageTime;
+        private TextView tvRecentMessage;
+        ArrayList<String> selectedInterests;
+
+        public ConversationViewHolder(@NonNull View itemView, ArrayList<String> selectedInterests) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            tvLastMessageTime = itemView.findViewById(R.id.tvLastMessageTime);
+            tvRecentMessage = itemView.findViewById(R.id.tvRecentMessage);
+
+            this.selectedInterests = selectedInterests;
+        }
+
+        public void bind(ConversationResponse conversation) {
+            String username = "";
+            List<Participant> participantList = conversation.getParticipants();
+            if(participantList.size() == 1){
+                username = participantList.get(0).getUser().getUsername();
+            }else{
+                ArrayList<String> usernames = new ArrayList<>();
+                for(Participant p : participantList){
+                    usernames.add(p.getUser().getUsername());
+                }
+                if(usernames.size() <= 2){
+                    username = String.join(", ", usernames);
+                }else{
+                    int otherUserCount = usernames.size()-2;
+                    username = usernames.get(0) + ", " + usernames.get(1) + " +" + otherUserCount + " others";
+                }
+            }
+
+            tvUsername.setText(username);
+            tvRecentMessage.setText(conversation.getLastMessage().getMessage());
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/langchat/MainActivity.java
+++ app/src/main/java/com/example/langchat/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.langchat;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import java.util.ArrayList;
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -19,7 +22,6 @@ import retrofit2.Response;
 import com.example.langchat.API.RetrofitClient;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
@@ -33,39 +35,44 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-            ArrayList<ConversationResponse> conversations = new ArrayList<>();
+        ArrayList<ConversationResponse> conversations = new ArrayList<>();
 
-                Call<List<ConversationResponse>> call = RetrofitClient.getInstance()
-                        .getAPI().getUsersConversations(1);
+        RecyclerView recycler = findViewById(R.id.recyclerView);
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        ConversationAdapter adapter = new ConversationAdapter(this, conversations);
+        recycler.setAdapter(adapter);
 
-                call.enqueue(new Callback<List<ConversationResponse>>() {
-                    @Override
-                    public void onResponse(Call<List<ConversationResponse>> call, Response<List<ConversationResponse>> response) {
-                        if(!response.isSuccessful()){
-                            return;
-                        }
-                        conversations.addAll(response.body());
+        Call<List<ConversationResponse>> call = RetrofitClient.getInstance()
+                .getAPI().getUsersConversations(1);
 
+        call.enqueue(new Callback<List<ConversationResponse>>() {
+            @Override
+            public void onResponse(Call<List<ConversationResponse>> call, Response<List<ConversationResponse>> response) {
+                if (!response.isSuccessful()) {
+                    return;
+                }
+                conversations.addAll(response.body());
+                adapter.notifyDataSetChanged();
 
-                        for(ConversationResponse convo : conversations){
-                            System.out.println("---- CONVO -----");
-                            for(Participant user : convo.getParticipants()){
-                                System.out.println("Participants: " + user.getUser().getUsername());
-                            }
-                            LastMessage lastMessage = convo.getLastMessage();
-                            if(lastMessage != null){
-                                // TODO; check local cache for lastMessage.getID() and the translation
-                                System.out.println("Last message: " + lastMessage.getMessage());
-                            }else{
-                                System.out.println("NULL LAST MESAGES");
-                            }
-                        }
+                for (ConversationResponse convo : conversations) {
+                    System.out.println("---- CONVO -----");
+                    for (Participant user : convo.getParticipants()) {
+                        System.out.println("Participants: " + user.getUser().getUsername());
                     }
-
-                    @Override
-                    public void onFailure(Call<List<ConversationResponse>> call, Throwable throwable) {
-
+                    LastMessage lastMessage = convo.getLastMessage();
+                    if (lastMessage != null) {
+                        // TODO; check local cache for lastMessage.getID() and the translation
+                        System.out.println("Last message: " + lastMessage.getMessage());
+                    } else {
+                        System.out.println("NULL LAST MESAGES");
                     }
-                });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<ConversationResponse>> call, Throwable throwable) {
+
+            }
+        });
     }
 }
