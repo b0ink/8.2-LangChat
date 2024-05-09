2125bcd669375dd8886fe087d243c786a75cbcdb
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-09 20:19:14 +1000

    Format

--- LangChat-API/controllers/Utility.js
+++ LangChat-API/controllers/Utility.js
@@ -6,44 +6,43 @@ const Message = db.messages;
 const Participant = db.participants;
 const language = db.participants;
 
-
 /**
  * Retrieve all conversations a user is a participant in
- * 
- * @param INTEGER user's ID 
+ *
+ * @param INTEGER user's ID
  * @returns An array of conversation IDs
  */
 module.exports.GetUsersConversations = async (user_id) => {
     const participantIds = await Participant.findAll({
         where: {
-            user_id: user_id
+            user_id: user_id,
         },
-        attributes: ['conversation_id']
+        attributes: ["conversation_id"],
     });
 
-    const conversationIds = participantIds.map(participant => participant.conversation_id);
+    const conversationIds = participantIds.map((participant) => participant.conversation_id);
 
     return conversationIds;
-}
-
+};
 
 /**
- * 
- * @param INTEGER conversation_id 
+ *
+ * @param INTEGER conversation_id
  * @returns An array of Message objects
  */
 module.exports.GetConversationMessages = async (conversation_id) => {
     const messages = await Message.findAll({
         where: {
-            conversation_id: conversation_id
+            conversation_id: conversation_id,
         },
-        include: [{
-            model: User,
-            as: 'user', 
-            attributes: ['username']
-        }]
+        include: [
+            {
+                model: User,
+                as: "user",
+                attributes: ["username"],
+            },
+        ],
     });
-
 
     // messages.forEach(message => {
     //     console.log('---------------------');
@@ -55,51 +54,52 @@ module.exports.GetConversationMessages = async (conversation_id) => {
     // });
 
     return messages;
-}
+};
 
 /**
- * 
- * @param INTEGER conversation_id 
+ *
+ * @param INTEGER conversation_id
  * @returns Message object
  */
 module.exports.GetMostRecentConversationMessage = async (conversation_id) => {
     const message = await Message.findOne({
         where: {
-            conversation_id: conversation_id
+            conversation_id: conversation_id,
         },
-        order: [['createdAt', 'DESC']],
-        include: [{
-            model: User,
-            as: 'user', 
-            attributes: ['username']
-        }]
+        order: [["createdAt", "DESC"]],
+        include: [
+            {
+                model: User,
+                as: "user",
+                attributes: ["username"],
+            },
+        ],
     });
 
     return message;
-}
-
+};
 
 module.exports.SendMessage = async (sender_id, conversation_id, message) => {
     const user = await User.findByPk(sender_id);
 
-    if(!user){
+    if (!user) {
         console.log(`Sender id:${sender_id} does not exist`);
         return;
     }
 
     const conversationIds = await this.GetUsersConversations(user.id);
-    if(!conversationIds.includes(conversation_id)){
+    if (!conversationIds.includes(conversation_id)) {
         console.log(conversationIds, conversation_id);
-        console.log(`SendMessage: Sender is not a part of this conversation`)
-        return ;
+        console.log(`SendMessage: Sender is not a part of this conversation`);
+        return;
     }
 
     const newMesssage = await Message.create({
         conversation_id,
         sender_id,
-        message
+        message,
     });
-    
+
     console.log(newMesssage.toJSON());
     return newMesssage;
-}
+};
