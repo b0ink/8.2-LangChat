8ce9da679cc4d98b0f2a4c0e5fd0f497d9cda5c5
b0ink <40929320+b0ink@users.noreply.github.com>
2024-05-20 21:21:17 +1000

    Formatting

--- LangChat-API/controllers/conversation.controller.js
+++ LangChat-API/controllers/conversation.controller.js
@@ -19,24 +19,24 @@ exports.addParticipant = async (req, res) => {
 
     // Check if calling user is part of converesation
     const usersConversations = await Utility.GetUsersConversations(user.id);
-    if(!usersConversations.includes(conversationId)){
+    if (!usersConversations.includes(conversationId)) {
         return res.status(401);
     }
-    
+
     const userToAdd = await User.findOne({
         where: {
-            username: usernameToAdd
-        }
+            username: usernameToAdd,
+        },
     });
-    
+
     // Check if user exists
-    if(!userToAdd){
-        return res.status(404).json("User does not exist")
+    if (!userToAdd) {
+        return res.status(404).json("User does not exist");
     }
 
     // Check if adding user is part of converesation already
     const userToAddConversations = await Utility.GetUsersConversations(userToAdd.id);
-    if(userToAddConversations.includes(conversationId)){
+    if (userToAddConversations.includes(conversationId)) {
         return res.status(401).json("User is already part of the conversation");
     }
 
@@ -45,17 +45,14 @@ exports.addParticipant = async (req, res) => {
     const newParticipant = await Participant.create({
         conversation_id: conversationId,
         user_id: userToAdd.id,
-        preferredLanguage: userToAdd.defaultPreferredLanguage
+        preferredLanguage: userToAdd.defaultPreferredLanguage,
     });
 
-    if(newParticipant){
+    if (newParticipant) {
         //TODO: add system message to notify new user joining
         return res.status(200).json("Added user to conversation");
     }
-
-
-}
-
+};
 
 exports.findParticipants = async (req, res) => {
     const user = req.user;
@@ -63,7 +60,7 @@ exports.findParticipants = async (req, res) => {
 
     // Check if user is part of converesation
     const usersConversations = await Utility.GetUsersConversations(user.id);
-    if(!usersConversations.includes(conversationId)){
+    if (!usersConversations.includes(conversationId)) {
         return res.status(401);
     }
 
@@ -72,25 +69,24 @@ exports.findParticipants = async (req, res) => {
     let Users = [];
     // Add the calling user to the top of the list
     Users.push({
-        username: user.username
+        username: user.username,
     });
 
-    for(let u of Participants){
-
-        if(u.user.username===user.username){
-            Users[0]['preferredLanguage'] = u.preferredLanguage;
+    for (let u of Participants) {
+        if (u.user.username === user.username) {
+            Users[0]["preferredLanguage"] = u.preferredLanguage;
             continue;
         }
 
         Users.push({
-            username: u.user.username
+            username: u.user.username,
         });
     }
 
-    console.log(Users)
+    console.log(Users);
 
     return res.json(Users);
-}
+};
 
 exports.findMessages = async (req, res) => {
     const user = req.user;
@@ -98,10 +94,10 @@ exports.findMessages = async (req, res) => {
     const lastMessageId = parseInt(req.params.lastMessageId);
 
     const usersConversations = await Utility.GetUsersConversations(user.id);
-    if(!usersConversations.includes(conversationId)){
+    if (!usersConversations.includes(conversationId)) {
         return res.status(401);
     }
-    console.log("finding messages in ", conversationId, 'offsetting by', lastMessageId)
+    console.log("finding messages in ", conversationId, "offsetting by", lastMessageId);
 
     const Messages = await Utility.GetConversationMessages(user, conversationId, lastMessageId);
 
@@ -116,32 +112,32 @@ exports.saveUsersLanguage = async (req, res) => {
     const language = req.body.language;
 
     const usersConversations = await Utility.GetUsersConversations(user.id);
-    if(!usersConversations.includes(conversationId)){
+    if (!usersConversations.includes(conversationId)) {
         return res.status(401);
     }
 
     //TODO: validate that `language` exists in the database
-    
 
-    const [numberOfAffectedRows, affectedRows] = await Participant.update({
-            preferredLanguage: language
-        },{
+    const [numberOfAffectedRows, affectedRows] = await Participant.update(
+        {
+            preferredLanguage: language,
+        },
+        {
             where: {
                 conversation_id: conversationId,
-                user_id: user.id
+                user_id: user.id,
             },
             returning: true,
-            plain: true
+            plain: true,
         }
-    )
-    
-    if(affectedRows > 0){
-        return res.json(true)
-    }else{
-        return res.json(false)
-    }
-}
+    );
 
+    if (affectedRows > 0) {
+        return res.json(true);
+    } else {
+        return res.json(false);
+    }
+};
 
 exports.sendMessage = async (req, res) => {
     const conversationId = parseInt(req.params.conversationId);
@@ -152,27 +148,27 @@ exports.sendMessage = async (req, res) => {
 
     // TODO: should be unique to the (in this case) participants of active converation
     // TODO: so a custom key defined by userid+conversationId?
-    console.log('beggingin send message', conversationId, message, user)
+    console.log("beggingin send message", conversationId, message, user);
 
     try {
         const newMessage = await Utility.SendMessage(user.id, conversationId, message);
-        
+
         TranslationService(user, conversationId, newMessage);
 
         return res.json(newMessage);
     } catch (error) {
-        console.log(error)
+        console.log(error);
         return res.status(500);
     }
 };
 
 // reponse of sendMessage is used to insert the new bubble client-side, but client should not wait for server-side translations ifrst
-async function TranslationService(user, conversationId, newMessage){
+async function TranslationService(user, conversationId, newMessage) {
     const queue_name = `messages_${conversationId}`;
 
     const conversationParticipants = await Utility.GetConversationParticipants(conversationId);
-    for(let participant of conversationParticipants){
-        if(participant.user_id === user.id){
+    for (let participant of conversationParticipants) {
+        if (participant.user_id === user.id) {
             // Original author will only see their original message
             continue;
         }
@@ -182,18 +178,15 @@ async function TranslationService(user, conversationId, newMessage){
         const translation = await Translation.create({
             message_id: newMessage.id,
             language: usersLanguage,
-            message: translatedMessage
+            message: translatedMessage,
         });
 
         const response = await Utility.NotifyNewMessage(`messages_${conversationId}_${participant.user_id}`);
-
     }
-
 
     // let translatedMesage = {...newMessage};
     // translatedMesage.message = await Utility.TranslateMessage(translatedMesage.message, "spanish");
 }
-
 
 exports.translateMessage = async (req, res) => {
     const user = req.user;
@@ -203,7 +196,7 @@ exports.translateMessage = async (req, res) => {
     const usersLanguage = req.body.usersLanguage;
 
     const message = await db.messages.findByPk(messageId);
-    if(message == null){
+    if (message == null) {
         return res.status(404);
     }
 
@@ -214,15 +207,14 @@ exports.translateMessage = async (req, res) => {
 
     //TODO: provide context to llama by retrieving the last 10 messages in the conversation and embed in prompt
     const conversations = await Utility.GetUsersConversations(user.id);
-    if(!conversations.includes(message.conversation_id)){
+    if (!conversations.includes(message.conversation_id)) {
         return res.status(401);
     }
 
     //TODO: validate usersLangage
     const translation = await Utility.TranslateMessage(message.message, usersLanguage);
 
-
-      const translatedMessage = {
+    const translatedMessage = {
         id: message.id,
         conversation_id: message.conversation_id,
         sender_id: message.sender_id,
@@ -230,15 +222,14 @@ exports.translateMessage = async (req, res) => {
         createdAt: message.createdAt,
         updatedAt: message.updatedAt,
         user: {
-            username: user.username
-        }
-    }
+            username: user.username,
+        },
+    };
     translatedMessage.message = translation;
-    console.log('translatedMessage')
-    console.log(translatedMessage)
+    console.log("translatedMessage");
+    console.log(translatedMessage);
 
     return res.status(200).json(translatedMessage);
 
-    console.log(typeof(conversation_id));
-
+    console.log(typeof conversation_id);
 };
